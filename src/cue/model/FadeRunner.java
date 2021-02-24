0e9a3d81d1448d429b0ddd9787a10ea5f4097ac3
crewbindrup <crewbindrup@gmail.com>
2021-02-23 20:53:53 -0700

    Added FadeRunner (Thread based Fade command.)

---
+++ src/cue/model/FadeRunner.java
@@ -0,0 +1,56 @@
+package cue.model;
+
+import java.awt.Color;
+
+import javax.swing.JTextArea;
+
+public class FadeRunner extends RunnerObj {
+	
+	private final JTextArea panel;
+	private final int time;
+	private final int[] endColor;
+	private Thread t;
+	
+	public FadeRunner(JTextArea panel, int time, int[] endColor) {
+		this.panel = panel;
+		this.time = time;
+		this.endColor = endColor;
+	}
+	
+	public void execute()
+	{
+		t = new Thread(new Runnable() {
+			
+			private long startMilis = System.currentTimeMillis();
+			private long endMilis = startMilis + time;
+			private Color start = panel.getBackground();
+			private int[] startColor = new int[] {start.getRed(), start.getGreen(), start.getBlue()};
+			
+			@Override
+			public void run() {
+				
+				while (System.currentTimeMillis() < endMilis)
+				{
+					double factor = (double)(System.currentTimeMillis() - startMilis) / (double)time;
+					panel.setBackground(new Color(	startColor[0] + (int)(factor * (double)(endColor[0] - startColor[0])),
+														startColor[1] + (int)(factor * (double)(endColor[1] - startColor[1])),
+														startColor[2] + (int)(factor * (double)(endColor[2] - startColor[2]))));
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		});
+		t.start();
+	}
+	
+	public boolean isAlive()
+	{
+		return t.isAlive();
+	}
+	
+}
