96f05ffc19a4886da90412c77365f4cf38e792cc
PennyJim <christopher@pepperhills.org>
2021-04-29 08:02:22 -0600

    Added JavaDoc to the Keyword

--- src/cue/model/Keyword.java
+++ src/cue/model/Keyword.java
@@ -2,9 +2,24 @@ package cue.model;
 
 public abstract class Keyword
 {
-	public Keyword(Parser parser, String inputs) {}		//Default constructor
+	/**
+	 * The required constructor to any inherited keyword 
+	 * @param parser The object running the cues
+	 * @param inputs A string of what is given along with the keyword
+	 */
+	public Keyword(Parser parser, String inputs) {}
+	/**
+	 * Take a step forward in the keyword's execution
+	 */
 	public abstract void step();
+	/**
+	 * @return Whether or not there are any steps left to take
+	 */
 	public abstract boolean hasStep();
 	
+	/**
+	 * Returns a string of what is wrong with the constructed inputs
+	 * @return Either an empty string or a string describing what went wrong
+	 */
 	public abstract String validateInputs();
 }
