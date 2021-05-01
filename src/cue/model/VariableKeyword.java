74004acd6e6f4ecd72e51f0db51d3e2748e89eab
PennyJim <christopher@pepperhills.org>
2021-05-01 15:06:59 -0600

    Validated the variable inputs

--- src/cue/model/VariableKeyword.java
+++ src/cue/model/VariableKeyword.java
@@ -13,6 +13,45 @@ public class VariableKeyword extends Keyword
 	public VariableKeyword(Parser parser, String inputs)
 	{
 		super(parser, inputs);
+		Scanner input = new Scanner(inputs);
+		
+		varName = input.next();
+		
+		try
+		{
+			double a = Double.parseDouble(input.next());
+			
+			if (input.hasNext())
+			{
+				try
+				{
+					double b = Double.parseDouble(input.next());
+					double c = Double.parseDouble(input.next());
+					
+					varValue = new Variable(a, b, c);
+					validateString = "";
+				}
+				catch (NumberFormatException e)
+				{
+					validateString = "Not all vaulues are valid numbers";
+				}
+				catch (NoSuchElementException e)
+				{
+					validateString = "Need one or three values, not two";
+				}
+			}
+			else
+			{
+				varValue = new Variable(a);
+				validateString = "";
+			}
+		}
+		catch (NumberFormatException e)
+		{
+			validateString = "Value is not a valid number";
+		}
+		
+		input.close();
 	}
 
 	@Override
