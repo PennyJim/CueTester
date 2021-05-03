a0fd4b77c35b7cac0398aa18d6a11055da9a8b2f
PennyJim <christopher@pepperhills.org>
2021-05-03 08:53:49 -0600

    Added JavaDoc to HoldKeyword

--- src/cue/model/HoldKeyword.java
+++ src/cue/model/HoldKeyword.java
@@ -5,15 +5,27 @@ import cue.controller.CueController;
 public class HoldKeyword extends Keyword
 {
 
+	/**
+	 * Only constructs HoldKeyword
+	 * 
+	 * @param controller
+	 * @param inputs
+	 */
 	public HoldKeyword(CueController controller, String inputs)
 	{
 		super(controller, inputs);
 		validateString = "";
 	}
 
+	/**
+	 * Empty Function
+	 */
 	@Override
 	public void step() {}
 
+	/**
+	 * To keep {@link CueThread}'s attention, returns true.
+	 */
 	@Override
 	public boolean hasStep()
 	{
