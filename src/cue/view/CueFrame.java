c6c148615d5d4b70eaa3be864c8adf2862cebee1
PennyJim <christopher@pepperhills.org>
2021-05-18 20:21:27 -0600

    Made the window resizable and relocatable

--- src/cue/view/CueFrame.java
+++ src/cue/view/CueFrame.java
@@ -109,6 +109,10 @@ public class CueFrame extends JFrame
 		}
 		comps[comps.length - 3].setForeground(null);
 		
+		CueMouseListener mouseListener = new CueMouseListener(this);
+		this.addMouseListener(mouseListener);
+		this.addMouseMotionListener(mouseListener);
+		
 		
 		JFrame frame = this;
 		minimize.addActionListener(new AbstractAction()
@@ -127,6 +131,222 @@ public class CueFrame extends JFrame
 				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 			}
 		});
+	}
+	private class CueMouseListener implements MouseInputListener
+	{
+		CueFrame frame;
+		Point clickPoint;
+		int resizeDir;
 		
+		int x = -1;
+		int y = -1;
+		int width = -1;
+		int height = -1;
+		
+		public CueMouseListener(CueFrame frame)
+		{
+			this.frame = frame;
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {}
+
+		@Override
+		public void mousePressed(MouseEvent e)
+		{
+			clickPoint = e.getPoint();
+			resizeDir = getResizeDir(e.getPoint());
+			
+			x = frame.getX();
+			y = frame.getY();
+			width = frame.getWidth();
+			height = frame.getHeight();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e)
+		{
+			clickPoint = null;
+			resizeDir = -1;
+			
+			x = -1;
+			y = -1;
+			width = -1;
+			height = -1;
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
+
+		@Override
+		public void mouseDragged(MouseEvent e)
+		{			
+			int x = this.x;
+			int y = this.y;
+			int width = this.width;
+			int height = this.height;
+			
+			switch (resizeDir)
+			{
+				default:
+					double xChange;
+					double yChange;
+					break;
+				
+				case -1:
+					x = e.getXOnScreen() - clickPoint.x;
+					y = e.getYOnScreen() - clickPoint.y;
+					break;
+				case 0: //N
+					yChange = clickPoint.getY() - e.getY();
+					height += yChange;
+					y -= yChange;
+//					clickPoint.setLocation(x, y - yChange);
+					break;
+				case 1: //S
+					yChange = clickPoint.getY() - e.getY();
+					height -= yChange;
+					break;
+				case 2: //W
+					xChange = clickPoint.getX() - e.getX();
+					width += xChange;
+					x -= xChange;
+//					clickPoint.setLocation(x - xChange, y);
+					break;
+				case 3: //E
+					xChange = clickPoint.getX() - e.getX();
+					width -= xChange;
+					break;
+				case 4: //NW
+					yChange = clickPoint.getY() - e.getY();
+					height += yChange;
+					y -= yChange; 
+					
+					xChange = clickPoint.getX() - e.getX();
+					width += xChange;
+					x += xChange;
+//					clickPoint.setLocation(x - xChange, y - yChange);
+					break;
+				case 5: //NE
+					yChange = clickPoint.getY() - e.getY();
+					height += yChange;
+					y -= yChange;
+					
+					xChange = clickPoint.getX() - e.getX();
+					width -= xChange;
+//					clickPoint.setLocation(x, y - yChange);
+					break;
+				case 6: //SW
+					yChange = clickPoint.getY() - e.getY();
+					height -= yChange;
+					
+					xChange = clickPoint.getX() - e.getX();
+					width += xChange;
+					x -= xChange;
+//					clickPoint.setLocation(x - xChange, y);
+					break;
+				case 7: //SE
+					yChange = clickPoint.getY() - e.getY();
+					height -= yChange;
+					
+					xChange = clickPoint.getX() - e.getX();
+					width -= xChange;
+					break;
+			}
+			
+			if (resizeDir != -1)
+			{
+				if (width < 340) { width = 340; }
+				if (height < 230) { height = 230; }
+				frame.setSize(width, height);
+			}
+			frame.setLocation(x, y);
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e)
+		{
+			int dir = getResizeDir(e.getPoint());
+			Cursor cursor = Cursor.getDefaultCursor();
+			if (dir != -1) { cursor = Cursor.getPredefinedCursor(cursors[dir]); }
+			setCursor(cursor);
+		}
+
+	    int[] cursors = {
+	        Cursor.N_RESIZE_CURSOR,
+	        Cursor.S_RESIZE_CURSOR,
+	        Cursor.W_RESIZE_CURSOR,
+	        Cursor.E_RESIZE_CURSOR,
+	        Cursor.NW_RESIZE_CURSOR,
+	        Cursor.NE_RESIZE_CURSOR,
+	        Cursor.SW_RESIZE_CURSOR,
+	        Cursor.SE_RESIZE_CURSOR
+	    };
+		
+		private int getResizeDir(Point point)
+		{
+			Rectangle rect = frame.getBounds();
+			
+			boolean isEW = Math.min(point.getX(), rect.getWidth() - point.getX()) < 5.0;
+			boolean isNS = Math.min(point.getY(), rect.getHeight() - point.getY()) < 5.0;
+			
+			boolean isSouth = point.getY() >= rect.getHeight() / 2.0;
+			boolean isEast = point.getX() >= rect.getWidth() / 2.0;
+			
+//			double resultX = Math.min(point.getX(), rect.getWidth() - point.getX());
+//			double resultY = Math.min(point.getY(), rect.getHeight() - point.getY());
+//			System.out.println("X:  min: " + 0 + "\tpoint: " + point.getX() + "\tmax: " + rect.getWidth() + "\tresult: " + resultX);
+//			System.out.println("Y:  min: " + 0 + "\tpoint: " + point.getY() + "\tmax: " + rect.getHeight() + "\tresult: " + resultY);
+//			System.out.println("isEW\tisNS\tisSouth\tisEast");
+//			System.out.println(isEW + "\t" + isNS + "\t" + isSouth + "\t" + isEast);
+			
+			int dir = -1;
+			
+			int bools = 0b0000;
+			if (isNS) { bools |= 0b1000; }
+			if (isEW) { bools |= 0b0100; }
+			if (isSouth) {bools |= 0b0010; }
+			if (isEast) {bools |= 0b0001; }
+			
+			switch (bools)
+			{
+				case 0b1000: //N
+				case 0b1001:
+					dir = 0;
+					break;
+				case 0b1010: //S
+				case 0b1011:
+					dir = 1;
+					break;
+				case 0b0100: //E
+				case 0b0110:
+					dir = 2;
+					break;
+				case 0b0101: //W
+				case 0b0111:
+					dir = 3;
+					break;
+				case 0b1100: //NE
+					dir = 4;
+					break;
+				case 0b1101: //NW
+					dir = 5;
+					break;
+				case 0b1110: //SE
+					dir = 6;
+					break;
+				case 0b1111: //SW
+					dir = 7;
+					break;
+				default:
+					dir = -1;
+					break;
+			}
+			
+			return dir;
+		}
 	}
 }
